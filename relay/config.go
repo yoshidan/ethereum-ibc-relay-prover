92736d0717881e77ac2fb33d7923f1e94551a10a
yoshidan <naohiro.y@gmail.com>
2026-05-06 14:41:39 +0900

    support glamsterdam

--- relay/config.go
+++ relay/config.go
@@ -5,64 +5,11 @@ import (
 	"time"
 
 	"github.com/datachainlab/ethereum-ibc-relay-chain/pkg/relay/ethereum"
-	lctypes "github.com/datachainlab/ethereum-ibc-relay-prover/light-clients/ethereum/types"
+	lcrelay "github.com/datachainlab/ethereum-light-client-types/prover/relay"
+	lctypes "github.com/datachainlab/ethereum-light-client-types/prover/types"
 	"github.com/hyperledger-labs/yui-relayer/core"
 	"github.com/hyperledger-labs/yui-relayer/coreutil"
 	"github.com/hyperledger-labs/yui-relayer/otelcore"
-)
-
-const (
-	Mainnet = "mainnet"
-	Minimal = "minimal"
-	Sepolia = "sepolia"
-)
-
-const (
-	MAINNET_PRESET_SYNC_COMMITTEE_SIZE = 512
-	MINIMAL_PRESET_SYNC_COMMITTEE_SIZE = 32
-)
-
-const (
-	Altair    = "altair"
-	Bellatrix = "bellatrix"
-	Capella   = "capella"
-	Deneb     = "deneb"
-	Electra   = "electra"
-	Fulu      = "fulu"
-)
-
-var (
-	AltairSpec = lctypes.ForkSpec{
-		FinalizedRootGindex:        105,
-		CurrentSyncCommitteeGindex: 54,
-		NextSyncCommitteeGindex:    55,
-	}
-	BellatrixSpec = lctypes.ForkSpec{
-		FinalizedRootGindex:               AltairSpec.FinalizedRootGindex,
-		CurrentSyncCommitteeGindex:        AltairSpec.CurrentSyncCommitteeGindex,
-		NextSyncCommitteeGindex:           AltairSpec.NextSyncCommitteeGindex,
-		ExecutionPayloadGindex:            25,
-		ExecutionPayloadStateRootGindex:   18,
-		ExecutionPayloadBlockNumberGindex: 22,
-	}
-	CapellaSpec = BellatrixSpec
-	DenebSpec   = lctypes.ForkSpec{
-		FinalizedRootGindex:               CapellaSpec.FinalizedRootGindex,
-		CurrentSyncCommitteeGindex:        CapellaSpec.CurrentSyncCommitteeGindex,
-		NextSyncCommitteeGindex:           CapellaSpec.NextSyncCommitteeGindex,
-		ExecutionPayloadGindex:            CapellaSpec.ExecutionPayloadGindex,
-		ExecutionPayloadStateRootGindex:   34,
-		ExecutionPayloadBlockNumberGindex: 38,
-	}
-	ElectraSpec = lctypes.ForkSpec{
-		FinalizedRootGindex:               169,
-		CurrentSyncCommitteeGindex:        86,
-		NextSyncCommitteeGindex:           87,
-		ExecutionPayloadGindex:            DenebSpec.ExecutionPayloadGindex,
-		ExecutionPayloadStateRootGindex:   DenebSpec.ExecutionPayloadStateRootGindex,
-		ExecutionPayloadBlockNumberGindex: DenebSpec.ExecutionPayloadBlockNumberGindex,
-	}
-	FuluSpec = ElectraSpec
 )
 
 var _ core.ProverConfig = (*ProverConfig)(nil)
@@ -108,7 +55,7 @@ func (prc ProverConfig) Validate() error {
 	}
 	for hf := range prc.MinimalForkSched {
 		switch hf {
-		case Altair, Bellatrix, Capella, Deneb, Electra, Fulu:
+		case lcrelay.Altair, lcrelay.Bellatrix, lcrelay.Capella, lcrelay.Deneb, lcrelay.Electra, lcrelay.Fulu, lcrelay.Gloas:
 			// OK
 		default:
 			return fmt.Errorf("config attribute \"minimal_fork_sched\" contains an unknown key: %s", hf)
@@ -135,129 +82,9 @@ func (prc *ProverConfig) GetMaxClockDrift() time.Duration {
 
 // NOTE the prover supports only the mainnet and minimal preset for now
 func (prc *ProverConfig) IsMainnetPreset() bool {
-	switch prc.Network {
-	case Mainnet, Sepolia:
-		return true
-	case Minimal:
-		return false
-	default:
-		panic(fmt.Sprintf("unknown network: %v", prc.Network))
-	}
+	return lcrelay.IsMainnetPreset(prc.Network)
 }
 
 func (prc *ProverConfig) getForkParameters() *lctypes.ForkParameters {
-	switch prc.Network {
-	case Mainnet:
-		return &lctypes.ForkParameters{
-			GenesisForkVersion: []byte{0, 0, 0, 0},
-			Forks: []*lctypes.Fork{
-				{
-					Version: []byte{1, 0, 0, 0},
-					Epoch:   74240,
-					Spec:    &AltairSpec,
-				},
-				{
-					Version: []byte{2, 0, 0, 0},
-					Epoch:   144896,
-					Spec:    &BellatrixSpec,
-				},
-				{
-					Version: []byte{3, 0, 0, 0},
-					Epoch:   194048,
-					Spec:    &CapellaSpec,
-				},
-				{
-					Version: []byte{4, 0, 0, 0},
-					Epoch:   269568,
-					Spec:    &DenebSpec,
-				},
-				{
-					Version: []byte{5, 0, 0, 0},
-					Epoch:   364032,
-					Spec:    &ElectraSpec,
-				},
-				// ref: https://github.com/ethereum/consensus-specs/blob/v1.6.0/configs/mainnet.yaml#L55-L57
-				{
-					Version: []byte{6, 0, 0, 0},
-					Epoch:   411392,
-					Spec:    &FuluSpec,
-				},
-			},
-		}
-	case Minimal:
-		return &lctypes.ForkParameters{
-			GenesisForkVersion: []byte{0, 0, 0, 1},
-			Forks: []*lctypes.Fork{
-				{
-					Version: []byte{1, 0, 0, 1},
-					Epoch:   prc.MinimalForkSched[Altair],
-					Spec:    &AltairSpec,
-				},
-				{
-					Version: []byte{2, 0, 0, 1},
-					Epoch:   prc.MinimalForkSched[Bellatrix],
-					Spec:    &BellatrixSpec,
-				},
-				{
-					Version: []byte{3, 0, 0, 1},
-					Epoch:   prc.MinimalForkSched[Capella],
-					Spec:    &CapellaSpec,
-				},
-				{
-					Version: []byte{4, 0, 0, 1},
-					Epoch:   prc.MinimalForkSched[Deneb],
-					Spec:    &DenebSpec,
-				},
-				{
-					Version: []byte{5, 0, 0, 1},
-					Epoch:   prc.MinimalForkSched[Electra],
-					Spec:    &ElectraSpec,
-				},
-				{
-					Version: []byte{6, 0, 0, 1},
-					Epoch:   prc.MinimalForkSched[Fulu],
-					Spec:    &FuluSpec,
-				},
-			},
-		}
-	case Sepolia:
-		return &lctypes.ForkParameters{
-			GenesisForkVersion: []byte{144, 0, 0, 105},
-			Forks: []*lctypes.Fork{
-				{
-					Version: []byte{144, 0, 0, 112},
-					Epoch:   50,
-					Spec:    &AltairSpec,
-				},
-				{
-					Version: []byte{144, 0, 0, 113},
-					Epoch:   100,
-					Spec:    &BellatrixSpec,
-				},
-				{
-					Version: []byte{144, 0, 0, 114},
-					Epoch:   56832,
-					Spec:    &CapellaSpec,
-				},
-				{
-					Version: []byte{144, 0, 0, 115},
-					Epoch:   132608,
-					Spec:    &DenebSpec,
-				},
-				{
-					Version: []byte{144, 0, 0, 116},
-					Epoch:   222464,
-					Spec:    &ElectraSpec,
-				},
-				// The metadata of Fulu Sepolia is from https://github.com/eth-clients/sepolia/blob/f9158732adb1a2a6440613ad2232eb50e7384c4f/metadata/config.yaml#L43-L45
-				{
-					Version: []byte{144, 0, 0, 117},
-					Epoch:   272640,
-					Spec:    &FuluSpec,
-				},
-			},
-		}
-	default:
-		panic(fmt.Sprintf("unknown network: %v", prc.Network))
-	}
+	return lcrelay.GetForkParameters(prc.Network, prc.MinimalForkSched)
 }
--- relay/execution.go
+++ relay/execution.go
@@ -2,50 +2,15 @@ package relay
 
 import (
 	"context"
-	"encoding/hex"
-	"math/big"
 
-	lctypes "github.com/datachainlab/ethereum-ibc-relay-prover/light-clients/ethereum/types"
-	"github.com/ethereum/go-ethereum/crypto"
+	lcrelay "github.com/datachainlab/ethereum-light-client-types/prover/relay"
+	lctypes "github.com/datachainlab/ethereum-light-client-types/prover/types"
 )
 
 func (pr *Prover) buildAccountUpdate(ctx context.Context, blockNumber uint64) (*lctypes.AccountUpdate, error) {
-	proof, err := pr.executionClient.GetProof(
-		ctx,
-		pr.ibcAddress,
-		nil,
-		big.NewInt(int64(blockNumber)),
-	)
-	if err != nil {
-		return nil, err
-	}
-	pr.GetLogger().InfoContext(ctx, "buildAccountUpdate: get proof", "block_number", blockNumber, "ibc_address", pr.ibcAddress.String(), "account_proof", hex.EncodeToString(proof.AccountProofRLP), "storage_hash", hex.EncodeToString(proof.StorageHash[:]))
-	return &lctypes.AccountUpdate{
-		AccountProof:       proof.AccountProofRLP,
-		AccountStorageRoot: proof.StorageHash[:],
-	}, nil
+	return lcrelay.BuildAccountUpdate(ctx, pr.executionClient, pr.ibcAddress, blockNumber)
 }
 
 func (pr *Prover) buildStateProof(ctx context.Context, path []byte, height int64) ([]byte, error) {
-	// calculate slot for commitment
-	storageKey := crypto.Keccak256Hash(append(
-		crypto.Keccak256Hash(path).Bytes(),
-		IBCCommitmentsSlot.Bytes()...,
-	))
-	storageKeyHex, err := storageKey.MarshalText()
-	if err != nil {
-		return nil, err
-	}
-
-	// call eth_getProof
-	stateProof, err := pr.executionClient.GetProof(
-		ctx,
-		pr.ibcAddress,
-		[][]byte{storageKeyHex},
-		big.NewInt(height),
-	)
-	if err != nil {
-		return nil, err
-	}
-	return stateProof.StorageProofRLP[0], nil
+	return lcrelay.BuildStateProof(ctx, pr.executionClient, pr.ibcAddress, path, height)
 }
